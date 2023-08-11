961183975def8bbf0a428909e014b085ca2f8892
NaumchukSV <130309363+NaumchukSV@users.noreply.github.com>
2023-08-11 22:31:24 +0300

    Completed task 09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -2,4 +2,16 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
+
+const body = document.querySelector('body');
+const btn = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+
+const changeBackgroundColor = (event) => {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  spanEl.textContent = color;
+};
+
+btn.addEventListener('click', changeBackgroundColor);
