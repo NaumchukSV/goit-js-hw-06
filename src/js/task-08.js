2dd4878038e1dc255b98f200a24646b87500af40
NaumchukSV <130309363+NaumchukSV@users.noreply.github.com>
2023-08-11 21:55:48 +0300

    Completed task 08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const formEl = document.querySelector(".login-form");
 
+const submitHandler = (event) => {
+  event.preventDefault();
+    const { email, password } = event.currentTarget.elements;
+    if (!email.value || !password.value) {
+        alert("Увага! Заповніть всі поля!");
+    }
+    else {
+    const info = {
+    email: email.value,
+    password: password.value,
+  };
+        console.log(info);
+        formEl.reset();
+    };
+    
+};
+
+formEl.addEventListener("submit", submitHandler);
