daefdd0d72f4526ec6d0ec27343060677a07e65b
NaumchukSV <130309363+NaumchukSV@users.noreply.github.com>
2023-08-11 14:12:39 +0300

    Completed task 02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+function render(arr) {
+  const list = document.querySelector('#ingredients');
+  
+  const elements = arr.map((item) => {
+    const liEl = document.createElement('li');
+    liEl.classList.add('item');
+    liEl.textContent = item;
+    return liEl;
+  });
+  
+  list.append(...elements);
+};
+
+render(ingredients);
